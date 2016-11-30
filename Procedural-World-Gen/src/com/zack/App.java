23f0d0e3f38d769a63b7f83cde465f7a9d82a47b
Zack Davidson <zackdavidson2014@gmail.com>
2016-11-30 11:47:08 +0000

    Nearly painting graphics

--- Procedural-World-Gen/src/com/zack/App.java
+++ Procedural-World-Gen/src/com/zack/App.java
@@ -1,11 +1,25 @@
 package com.zack;
 
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
+
+import com.zack.ui.WorldGUI;
+
 public class App
 {
 
-    public static void main(String... args)
+    public static void main(String... args) throws InvocationTargetException, InterruptedException
     {
-        
+        SwingUtilities.invokeAndWait(new Runnable()
+        {
+            
+            @Override
+            public void run()
+            {
+                new WorldGUI();
+            }
+        });
     }
 
 }
--- Procedural-World-Gen/src/com/zack/ui/WorldGUI.java
+++ Procedural-World-Gen/src/com/zack/ui/WorldGUI.java
@@ -1,20 +1,50 @@
 package com.zack.ui;
 
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.font.GraphicAttribute;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import com.zack.Constants;
 import com.zack.world.World;
+import com.zack.world.WorldTile;
 
 public class WorldGUI extends JFrame
 {
     private static final long serialVersionUID = -3484808758895456098L;
     private World world;
     
+    private JPanel panel = new JPanel();
+    private Canvas canvas = new Canvas();
+    
     public WorldGUI()
     {
         super(Constants.APPLICATION_NAME);
         this.setSize(Constants.APPLICATION_SIZE);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setContentPane(panel);
+        this.setVisible(true);
+        
+        panel.setLayout(null);
+        panel.add(canvas);
+        
+        paintCanvas(canvas.getGraphics());
+        
+        world = new World("test");
+        world.build();
+        
+        this.add(panel);
     }
+    
+    public void paintCanvas(Graphics g) {
+        for (WorldTile tile : world.getTiles())
+        {
+            g.setColor(Color.);
+        }
+    }
+    
+    
 
 }
